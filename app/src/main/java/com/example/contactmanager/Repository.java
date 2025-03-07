1e495fb0e7420138f348b03eb47afdf708de4cad
Hussein Yasser <119596187+Hussein2077@users.noreply.github.com>
2025-03-07 02:28:10 +0200

    view model

---
+++ app/src/main/java/com/example/contactmanager/Repository.java
@@ -0,0 +1,53 @@
+package com.example.contactmanager;
+
+
+import android.app.Application;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Repository {
+    private final ContactDAO contactDAO;
+    ExecutorService executor;
+    Handler handler;
+
+
+    public Repository(Application application) {
+
+        ContactDatabase contactDatabase = ContactDatabase.getInstance(application);
+        this.contactDAO = contactDatabase.getContactDAO();
+        executor = Executors.newSingleThreadExecutor();
+        handler = new Handler(Looper.getMainLooper());
+    }
+
+    public void addContact(Contacts contact){
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                // Execute this code asynchronously
+                // on separate thread
+                contactDAO.insert(contact);
+            }
+        });
+
+
+
+    }
+
+
+    public void deleteContact(Contacts contact){
+        executor.execute(() -> contactDAO.delete(contact));
+
+    }
+
+    public LiveData<List<Contacts>> getAllContacts(){
+        return contactDAO.getAllContacts();
+    }
+
+
+}
